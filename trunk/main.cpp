e4172fe448ffedf51ee26a42f8d76302d3744764
camille.oswald@gmail.com <camille.oswald@gmail.com@fa8e5bc0-ed13-1dc8-6383-4cafb5291e00>
2013-02-19 14:32:12 +0000

    Main with save image in rectangle

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -5,13 +5,16 @@
 #include <stdio.h>
 #include <sys/stat.h>
 
-
 using namespace std;
 using namespace cv;
 
-
 int main()
 {
+    //char* xmlFile = "Personnes.xml";
+	//Parser parser= Parser();
+	//parser.Parse(xmlFile);
+
+
 	CvCapture* capture;
 	IplImage* frame = 0;
 	char s[] = "picture.jpg";
@@ -19,6 +22,7 @@ int main()
 	CvHaarClassifierCascade* cascade = 0;
 	CvMemStorage *storage = 0;
 	CvSeq *faceRectSeq;
+    int nbFrame=0;
 
 	storage = cvCreateMemStorage(0);
 
@@ -33,36 +37,57 @@ int main()
 		cascade = (CvHaarClassifierCascade*) cvLoad(Filexml);
 	}
 
-
-	capture = cvCaptureFromCAM(0);
+    //Read the video stream
+	//capture = cvCaptureFromAVI("Test.avi");
 
 	while (true)
 	{
-		//Read the video stream
-		//capture = cvCaptureFromCAM(0);
+		capture = cvCaptureFromCAM(1);
 		frame = cvQueryFrame( capture );
 		// create a window to display detected faces
 		cvNamedWindow("Sample Program", CV_WINDOW_AUTOSIZE);
+		cvNamedWindow("Visage", CV_WINDOW_AUTOSIZE);
 		// display face detections
 		faceRectSeq = cvHaarDetectObjects(frame,cascade,storage,1.2, 3,CV_HAAR_DO_CANNY_PRUNING,cvSize(50,50));
+        CvRect *r;
 
 		for ( int i = 0; i < (faceRectSeq? faceRectSeq->total:0); i++ )
 		{
-			CvRect *r = (CvRect*)cvGetSeqElem(faceRectSeq,i);
+            r = (CvRect*)cvGetSeqElem(faceRectSeq,i);
 			CvPoint p1 = { r->x, r->y };
 			CvPoint p2 = { r->x + r->width, r->y + r->height };
 
 			// printf(" %d %d %d %d\n", r->x, r->y, r->width, r->height);
 			cvRectangle(frame,p1,p2,CV_RGB(0,255,0),1,4,0);
+
+			//Récupération de l'image à l'interieur du rectangle r
+        cvSetImageROI(frame, *r);
+        //Copie l'image dans le dossier Img avec le nom picture+NbFrame+.jpg
+        IplImage *subImg = cvCreateImage(cvGetSize(frame), frame->depth, frame->nChannels);
+        cvCopy(frame, subImg, NULL);
+        string Result;
+        char* str_Result[100];
+        ostringstream convert;
+        convert << nbFrame;
+        Result = "Img\\picture"+convert.str()+".jpg";
+
+        //Affiche l'image dans le rectangle et l'image dans son intégralité
+        cvSaveImage(Result.c_str(), subImg);
+        cvShowImage("Visage", subImg);
+        cvResetImageROI(frame);
+        cvShowImage("Sample Program", frame);
+
+
 		}
 
-		cvShowImage("Sample Program", frame);
+
 		int c = cvWaitKey(10);
 		if( (char)c == 27 )
 		{
 			cvSaveImage(s, frame);
 			exit(0);
 		}
+		nbFrame++;
 	}
 
 
