7c78c9def21712b6af28d3ca00e5706d7054de59
christopher.gnatto@gmail.com <christopher.gnatto@gmail.com@fa8e5bc0-ed13-1dc8-6383-4cafb5291e00>
2013-02-19 13:09:44 +0000

    Version TinyXML

--- trunk/src/Parser.cpp
+++ trunk/src/Parser.cpp
@@ -1,11 +1,18 @@
 #include "Parser.h"
 #include <iostream>
+#include "include/tinyxml.h"
+#include "include/tinystr.h"
 
 using namespace std;
 
 Parser::Parser()
 {
-    //ctor
+        TiXmlDocument doc("data/Personnes.xml");
+    if(!doc.LoadFile()){
+        cerr << "erreur lors du chargement" << endl;
+        cerr << "error #" << doc.ErrorId() << " : " << doc.ErrorDesc() << endl;
+        return 1;
+    }
 }
 
 Parser::~Parser()
